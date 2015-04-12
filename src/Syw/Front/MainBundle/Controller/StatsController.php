f092c4b74b8b33880eefa0c4f8651245a8e2c648
Alexander Löhner <alex.loehner@linux.com>
2015-04-12 19:56:02 +0200

    trying to get the highcharts theme working on the counter statistics...

--- src/Syw/Front/MainBundle/Controller/StatsController.php
+++ src/Syw/Front/MainBundle/Controller/StatsController.php
@@ -348,10 +348,12 @@ class StatsController extends BaseController
         }
         $series = array(
             array(
+                "type" => "area",
                 "name" => $this->get('translator')->trans('User Registrations'),
                 "data" => $data2
             ),
             array(
+                "type" => "area",
                 "name" => $this->get('translator')->trans('Machine Registrations'),
                 "data" => $data1
             )
