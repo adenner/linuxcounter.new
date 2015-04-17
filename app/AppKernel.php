02865f11080df0597d1f825af1202d7295d808ab
Alexander Löhner <alex.loehner@linux.com>
2015-04-17 21:16:33 +0200

    added adsense ads and modified the templates for some styling issues

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,8 @@ class AppKernel extends Kernel
             new Whisnet\IrcBotBundle\WhisnetIrcBotBundle(),
             new BladeTester\LightNewsBundle\BladeTesterLightNewsBundle(),
             new Redmonster\AnnouncementBundle\RedmonsterAnnouncementBundle(),
+            new Knp\Bundle\LastTweetsBundle\KnpLastTweetsBundle(),
+            new Inori\TwitterAppBundle\InoriTwitterAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
