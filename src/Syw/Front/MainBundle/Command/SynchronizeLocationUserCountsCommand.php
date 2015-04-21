9308f0c315c0f64ea8d238eabf676d0ec5e35a5b
Alexander Löhner <alex.loehner@linux.com>
2015-04-21 08:32:13 +0200

    fix

--- src/Syw/Front/MainBundle/Command/SynchronizeLocationUserCountsCommand.php
+++ src/Syw/Front/MainBundle/Command/SynchronizeLocationUserCountsCommand.php
@@ -23,7 +23,9 @@ class SynchronizeLocationUserCountsCommand extends ContainerAwareCommand
         $this
             ->setName('syw:synchronize:usercounts')
             ->setDescription('Synchronizes the user counts in the cities and countries tables')
-            ->setDefinition(array())
+            ->setDefinition(array(
+                new InputArgument('item', InputArgument::REQUIRED, 'the item to import')
+            ))
             ->setHelp(<<<EOT
 The <info>syw:synchronize:usercounts</info> command Synchronizes the user counts in the cities and countries tables.
 
@@ -36,36 +38,95 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $item = $input->getArgument('item');
+
+        $licotestdb = $this->getContainer()->get('doctrine.dbal.default_connection');
         $db = $this->getContainer()->get('doctrine')->getManager();
 
-        $c = 0;
-        $cities = $db->getRepository('SywFrontMainBundle:Cities')->findAll();
-        foreach ($cities as $city) {
-            $c++;
-            $userProfiles = null;
-            unset($userProfiles);
-            $userProfiles = $city->getUsers();
-            $city->setUserNum(sizeof($userProfiles));
-            $db->persist($city);
-            if ($c == 100) {
-                echo ".";
-                $c = 0;
-            }
+        if ($item == "cities") {
+            @exec("php app/console syw:synchronize:usercounts citiesbg >>import.log 2>&1 3>&1 4>&1 &");
+            exit(0);
         }
-        $db->flush();
 
-        $countries = $db->getRepository('SywFrontMainBundle:Countries')->findAll();
-        foreach ($countries as $country) {
-            $userProfiles = null;
-            unset($userProfiles);
-            $userProfiles = $country->getUsers();
-            $country->setUsersNum(sizeof($userProfiles));
-            $db->persist($country);
-            echo ".";
+
+        if ($item == "citiesbg") {
+            gc_collect_cycles();
+
+            if (true === file_exists('import.db')) {
+                $fp   = fopen('import.db', "r");
+                $data = fread($fp, 1024);
+                fclose($fp);
+                $fp = null;
+                unset($fp);
+                $dataar  = explode(" ", trim($data));
+                $start   = intval(trim($dataar[0]));
+                $counter = intval(trim($dataar[1]));
+            } else {
+                $nums     = $licotestdb->fetchAll('SELECT COUNT(id) AS num FROM cities');
+                $numusers = $nums[0]['num'];
+                $start    = 0; // $numusers;
+                $counter  = 0;
+            }
+            $itemsperloop = 200;
+
+            $z = 0;
+            $a = $start;
+            $c = 0;
+            $cities = $db->getRepository('SywFrontMainBundle:Cities')->findBy(array(), array('id' => 'ASC'), $itemsperloop, $a);
+            foreach ($cities as $city) {
+                $counter++;
+                gc_collect_cycles();
+
+                $userProfiles = null;
+                unset($userProfiles);
+                $userProfiles = $city->getUsers();
+                $num = sizeof($userProfiles);
+                $city->setUserNum(sizeof($num));
+                $db->persist($city);
+                if ($c == 100) {
+                    echo ".";
+                    $c = 0;
+                }
+
+                gc_collect_cycles();
+
+                $userProfiles = null;
+                unset($userProfiles);
+
+                $z++;
+
+                file_put_contents(
+                    "import.log",
+                    ">>> " . $counter . " | " . $z . " | " . $city->getId() . " | " . $num . " \n",
+                    FILE_APPEND
+                );
+
+                gc_collect_cycles();
+            }
+            file_put_contents('import.db', ($a + $itemsperloop) . " " . $counter);
+
+            $db->flush();
+            $licotestdb->close();
+            gc_collect_cycles();
+            @exec("php app/console syw:synchronize:usercounts cities >>import.log 2>&1 3>&1 4>&1 &");
+            exit(0);
         }
-        $db->flush();
-        echo "\n";
-        $output->writeln(sprintf('Usernumbers synchronized', ''));
+
+
+        if ($item == "countries") {
+            $countries = $db->getRepository('SywFrontMainBundle:Countries')->findAll();
+            foreach ($countries as $country) {
+                $userProfiles = null;
+                unset($userProfiles);
+                $userProfiles = $country->getUsers();
+                $country->setUsersNum(sizeof($userProfiles));
+                $db->persist($country);
+                echo ".";
+            }
+            $db->flush();
+            echo "\n";
+            $output->writeln(sprintf('Usernumbers synchronized', ''));
+        }
     }
 
     /**
@@ -73,6 +134,19 @@ EOT
      */
     protected function interact(InputInterface $input, OutputInterface $output)
     {
+        if (!$input->getArgument('item')) {
+            $item = $this->getHelper('dialog')->askAndValidate(
+                $output,
+                'Please enter a item:',
+                function ($item) {
+                    if (empty($item)) {
+                        throw new \Exception('item can not be empty');
+                    }
 
+                    return $item;
+                }
+            );
+            $input->setArgument('item', $item);
+        }
     }
 }
