8bc043173420f0b85a34c12f54582f3667f857ab
Alexander Löhner <alex.loehner@linux.com>
2015-04-25 22:35:04 +0200

    fix

--- src/Syw/Front/MainBundle/Command/ImportDistributionsCommand.php
+++ src/Syw/Front/MainBundle/Command/ImportDistributionsCommand.php
@@ -9,7 +9,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use FOS\UserBundle\Model\User;
 use Syw\Front\MainBundle\Entity\Distributions;
-use Syw\Front\MainBundle\Entity\DistributionsNew;
 
 /**
  *
@@ -41,7 +40,7 @@ EOT
         $qb = $this->getContainer()->get('doctrine.dbal.default_connection');
 
         $importlogfile = "import.distributions";
-        $filetoimport = "distributions.html";
+        $filetoimport  = "distributions.html";
 
         $content = file_get_contents($filetoimport);
 
@@ -49,28 +48,25 @@ EOT
         preg_match_all("`\s*\<p\>\s*(\<li\>\s*.*\s*\<\/li\>)\s*\<\/p\>\s*`isU", $content, $matches);
 
         foreach ($matches[1] as $match) {
-            $url            = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*([^\<]+)\s*\<\/a\>\s*\<\/b\>\s*.*`s", "$1", trim($match));
-            $name           = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*([^\<]+)\s*\<\/a\>\s*\<\/b\>\s*.*`s", "$2", trim($match));
-            $description    = preg_replace("`.*\<\/a\>.?\<br\>\s*([^\<]+)\s*\<\/li\>.*`sU", "$1", trim($match));
-            $url            = trim(str_replace("\n", " ", $url));
-            $name           = trim(str_replace("\n", " ", $name));
+            $url         = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*([^\<]+)\s*\<\/a\>\s*\<\/b\>\s*.*`s", "$1", trim($match));
+            $name        = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*([^\<]+)\s*\<\/a\>\s*\<\/b\>\s*.*`s", "$2", trim($match));
+            $description = preg_replace("`.*\<\/a\>.?\<br\>\s*([^\<]+)\s*\<\/li\>.*`sU", "$1", trim($match));
+            $url         = trim(str_replace("\n", " ", $url));
+            $name        = trim(str_replace("\n", " ", $name));
 
             if (strlen($name) >= 150 || strlen($url) >= 150) {
                 // something went wrong... the source seems to be not correctly formatted.
-                $url        = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*\<\/a\>\s*([^\<]+)\s*\<\/b\>\s*.*`s", "$1", trim($match));
-                $name       = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*\<\/a\>\s*([^\<]+)\s*\<\/b\>\s*.*`s", "$2", trim($match));
-                $url        = trim(str_replace("\n", " ", $url));
-                $name       = trim(str_replace("\n", " ", $name));
+                $url  = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*\<\/a\>\s*([^\<]+)\s*\<\/b\>\s*.*`s", "$1", trim($match));
+                $name = preg_replace("`.*\<b\>\s*\<a\s*href=\"([^\"]+)\"[^\>]*\>\s*\<\/a\>\s*([^\<]+)\s*\<\/b\>\s*.*`s", "$2", trim($match));
+                $url  = trim(str_replace("\n", " ", $url));
+                $name = trim(str_replace("\n", " ", $name));
             }
             if (preg_match("`\>$`", trim($description))) {
                 $description = strip_tags($description);
             }
 
-            $name = trim(html_entity_decode($name));
+            $name        = trim(html_entity_decode($name));
             $description = trim(html_entity_decode($description));
-
-            $parts = explode(" ", $name);
-            $first = $parts[0];
 
             $pattern = str_replace("-linux", "", strtolower(trim($name)));
             $pattern = str_replace("linux-", "", $pattern);
@@ -78,12 +74,13 @@ EOT
             $pattern = str_replace("-", " ", $pattern);
             $pattern = str_replace("_", " ", $pattern);
             $pattern = str_replace(" ", "%", $pattern);
-            $pattern = "%".$pattern."%";
+            $pattern = "%" . $pattern . "%";
+            $pattern = str_replace("%%", "%", $pattern);
 
-            echo "> ".$name." <  (".$pattern.") \n";
+            echo "> " . $name . " <  (" . $pattern . ") \n";
             $rows = null;
             unset($rows);
-            $rows = $qb->fetchAll("SELECT d.id, d.name FROM distributions d WHERE LOWER(d.name) LIKE '".addslashes($pattern)."'");
+            $rows = $qb->fetchAll("SELECT d.id, d.name FROM distributions d WHERE LOWER(d.name) LIKE '" . addslashes($pattern) . "'");
 
             if (false === isset($rows) || count($rows) <= 0) {
                 $distribution = null;
@@ -98,12 +95,11 @@ EOT
             } else {
                 $found = false;
                 foreach ($rows as $row) {
-                    $thisid = $row['id'];
+                    $thisid   = $row['id'];
                     $thisname = $row['name'];
 
-                    // echo "    Found:  ".$thisname." \n";
-
-                    if (strtolower($thisname) == strtolower($name)) {
+                    if ((strtolower($thisname) == strtolower($name)) ||
+                        (strtolower(str_replace(" ", "", $thisname)) == strtolower(str_replace(" ", "", $name)))) {
                         $distribution = null;
                         unset($distribution);
                         $distribution = $db->getRepository('SywFrontMainBundle:Distributions')->findOneBy(array("id" => $thisid));
@@ -115,10 +111,32 @@ EOT
                         break;
                     }
                 }
+
+                if ($found === false) {
+                    foreach ($rows as $row) {
+                        $thisid   = $row['id'];
+                        $thisname = $row['name'];
+                        $thisparts = explode(" ", strtolower($thisname));
+                        $parts     = explode(" ", strtolower($name));
+                        $diff = $this->arrayDiff($thisparts, $parts);
+                        if (count($diff) == 1) {
+                            $distribution = null;
+                            unset($distribution);
+                            $distribution = $db->getRepository('SywFrontMainBundle:Distributions')->findOneBy(array("id" => $thisid));
+                            $distribution->setDescription($description);
+                            $distribution->setUrl($url);
+                            $db->persist($distribution);
+                            $db->flush();
+                            $found = true;
+                            break;
+                        }
+                    }
+                }
+
                 if ($found === false) {
                     $distribution = null;
                     unset($distribution);
-                    $distribution = new DistributionsNew();
+                    $distribution = new Distributions();
                     $distribution->setName($name);
                     $distribution->setUrl(trim($url));
                     $distribution->setDescription($description);
@@ -130,6 +148,45 @@ EOT
 
             echo "======================================== \n";
         }
+    }
+
+    public function arrayDiff($ary_1, $ary_2)
+    {
+        // compare the value of 2 array
+        // get differences that in ary_1 but not in ary_2
+        // get difference that in ary_2 but not in ary_1
+        // return the unique difference between value of 2 array
+        $diff = array();
+
+        // get differences that in ary_1 but not in ary_2
+        foreach ($ary_1 as $v1) {
+            $flag = 0;
+            foreach ($ary_2 as $v2) {
+                $flag |= ($v1 == $v2);
+                if ($flag) {
+                    break;
+                }
+            }
+            if (!$flag) {
+                array_push($diff, $v1);
+            }
+        }
+
+        // get difference that in ary_2 but not in ary_1
+        foreach ($ary_2 as $v2) {
+            $flag = 0;
+            foreach ($ary_1 as $v1) {
+                $flag |= ($v1 == $v2);
+                if ($flag) {
+                    break;
+                }
+            }
+            if (!$flag && !in_array($v2, $diff)) {
+                array_push($diff, $v2);
+            }
+        }
+
+        return $diff;
     }
 
     /**
