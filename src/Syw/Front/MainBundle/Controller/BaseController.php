aea5bad6edea1411a686991c82ea7df80931d16f
Alexander Löhner <alex.loehner@linux.com>
2015-05-04 20:20:31 +0200

    added user online record

--- src/Syw/Front/MainBundle/Controller/BaseController.php
+++ src/Syw/Front/MainBundle/Controller/BaseController.php
@@ -250,6 +250,12 @@ class BaseController extends Controller
         ;
         $counts['guests'] = $qb->getQuery()->getSingleScalarResult();
 
+
+        $conn = $this->get('doctrine.dbal.default_connection');
+        $rows = $conn->fetchAll('SELECT max(num) AS num, timestamp FROM stats_online_users ORDER BY timestamp DESC limit 1');
+        $counts['highest']['count'] = $rows[0]['num'];
+        $counts['highest']['date']  = new \DateTime($rows[0]['timestamp']);
+
         return $counts;
     }
 
