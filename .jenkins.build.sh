32f5d2dd2561c00ffd4e64058bac49c297475234
Alexander Löhner <alex.loehner@linux.com>
2015-05-11 16:57:26 +0200

    jenkins build fix

--- .jenkins.build.sh
+++ .jenkins.build.sh
@@ -6,7 +6,7 @@ mkdir -p app/cache
 mkdir -p app/logs
 php app/console doctrine:database:create
 cd app/config/travis-sql
-for i in *; do mysql -uroot jenkinsbuild < ${i}; done;
+for i in *; do mysql -ujenkins -pjenkins jenkinsbuild < ${i}; done;
 cd ../../..
 php app/console doctrine:cache:clear-metadata
 php app/console cache:clear
