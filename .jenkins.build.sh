2e20c3a0139aee4bc08a7e399c925abe910464ac
Alexander Löhner <alex.loehner@linux.com>
2015-05-11 16:48:39 +0200

    added jenkins build script

---
+++ .jenkins.build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+composer install
+
+mkdir -p app/cache
+mkdir -p app/logs
+php app/console doctrine:database:create
+cd app/config/travis-sql
+for i in *; do mysql -uroot jenkinsbuild < ${i}; done;
+cd ../../..
+php app/console doctrine:cache:clear-metadata
+php app/console cache:clear
+php app/console assets:install --symlink web --no-debug
+php app/console assetic:dump
+php app/console asm:translations:dummy
+
+
+
+
+
+
+
+
