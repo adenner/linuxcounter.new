ae5b2601231e668156cfe104cd7edf12b7bd702e
Alexander Löhner <alex.loehner@linux.com>
2015-05-24 13:01:03 +0200

    some fixes

--- src/Syw/Front/MainBundle/Resources/public/js/page.js
+++ src/Syw/Front/MainBundle/Resources/public/js/page.js
@@ -182,3 +182,17 @@ _qevents.push({
 ga('create', 'UA-41913985-1', 'linuxcounter.net');
 ga('set', 'anonymizeIp', true);
 ga('send', 'pageview');
+
+<!-- Piwik -->
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//piwik.linuxcounter.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+<!-- End Piwik Code -->
+
